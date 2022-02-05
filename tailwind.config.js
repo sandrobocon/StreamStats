f4c587c1636af532df3a56054a56a6bb858dbbc4
Sandro Boçon <sandro_bocon@yahoo.com.br>
2022-02-05 16:21:56 -0300

    Install TailwindCSS & Livewire

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+    ],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
